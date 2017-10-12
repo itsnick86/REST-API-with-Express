aec6be464141472570665372906f8f15e4268366
Nick Edwards <nledwa01@gmail.com>
2017-10-12 07:52:11 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var jsonParser = require('body-parser').json;
+
+app.use(jsonParser());
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+    console.log('Express server is listening on port', port);
+});
+
