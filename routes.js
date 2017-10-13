73cb0a249c9e73e2b2d102fc1639e766846ec6a5
Nick Edwards <nledwa01@gmail.com>
2017-10-13 17:42:02 -0400

    Using Express, built Answer routes to POST, PUT, and DELETE answers as well as vote up or down for those answers.

--- routes.js
+++ routes.js
@@ -18,11 +18,54 @@ router.post('/', function(req, res){
   });
 });
 
-// GET /questions/:id
+// GET /questions/:qID
 // Return for specific questions
-router.get('/:id', function(req, res){
+router.get('/:qID', function(req, res){
   res.json({
-    response: "You sent me a GET request for ID " + req.params.id
+    response: "You sent me a GET request for ID " + req.params.qID
+  });
+});
+
+// POST /questions/:qID/answers
+// Return for creating answer
+router.post('/:qID/answers', function(req, res){
+  res.json({
+    response: "You sent me a POST request to /answers",
+    questionId: req.params.qID,
+    body: req.body
+  });
+});
+
+// PUT /questions/:qID/answers/:aID
+// Edit a specific answer
+router.put('/:qID/answers/:aID', function(req, res){
+  res.json({
+    response: "You sent me a POST request to /answers",
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    body: req.body
+  });
+});
+
+// DELETE /questions/:qID/answers/:aID
+// Delete a specific answer
+router.delete('/:qID/answers/:aID', function(req, res){
+  res.json({
+    response: "You sent me a DELETE request to /answers",
+    questionId: req.params.qID,
+    answerId: req.params.aID
+  });
+});
+
+// POST /questions/:qID/answers/:aID/vote-up
+// POST /questions/:qID/answers/:aID/vote-down
+// Vote on a specific answer
+router.post('/:qID/answers/:aID/vote-:dir', function(req, res){
+  res.json({
+    response: "You sent me a POST request to /vote-" + req.params.dir,
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    vote: req.params.dir
   });
 });
 
