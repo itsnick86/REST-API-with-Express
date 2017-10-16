cc3c59424a45660e013dc158cd73bfdaafa105e6
Nick Edwards <nledwa01@gmail.com>
2017-10-16 15:13:50 -0400

    Set up Questions and Answers schemas.

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ var logger = require('morgan');
 app.use(logger('dev'));
 app.use(jsonParser());
 
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; 
+mongoose.connect("mongodb://localhost:27017/qa");
+
+var db = mongoose.connection;
+
+db.on('error', function(){
+  console.error("connection error:", err);
+});
+
+db.once('open', function(){
+  console.log('db connection successful');
+});
+
 app.use('/questions', routes);
 
 // catch 404 and forward to error handler
